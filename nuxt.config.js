50bfce6d7837d07dec17c14b199dd3a1220b75b1
MutsukiOri <57990154+MutsukiOri@users.noreply.github.com>
2021-02-15 17:14:49 +0900

    nuetify import

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,6 +22,7 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     '~plugins/vue-scrollto'
+    
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -29,6 +30,7 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
+    '@nuxtjs/vuetify',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
