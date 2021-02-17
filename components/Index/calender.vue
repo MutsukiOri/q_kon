ce363c66456c6eaf46ce8d3bc55a36478ad6146a
MutsukiOri <57990154+MutsukiOri@users.noreply.github.com>
2021-02-17 22:19:33 +0900

    calender紐付け

--- components/Index/calender.vue
+++ components/Index/calender.vue
@@ -9,7 +9,7 @@
     <v-card-title>カレンダー
         </v-card-title>
     <v-container>
-        <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Asia%2FTokyo&amp;src=c3B3aW1kYXJAZ21haWwuY29t&amp;src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;src=ZmFtaWx5MDc2NTc3NDgwNjc5OTI1MDMyNjNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=amEuamFwYW5lc2UjaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&amp;color=%23039BE5&amp;color=%2333B679&amp;color=%234285F4&amp;color=%230B8043&amp;showTz=1&amp;showTabs=1&amp;showPrint=1&amp;showNav=1&amp;showTitle=0" style="border:solid 1px #777" width="100%" height="500" frameborder="0" scrolling="no"></iframe>
+        <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Asia%2FTokyo&amp;src=bmVjZWQ4Zjc0MWFuNTcwOHRmb2o3Z25nZGdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23F4511E" style="border:solid 1px #777" width="100%" height="500" frameborder="0" scrolling="no"></iframe>
     </v-container>
         
     </v-img>
