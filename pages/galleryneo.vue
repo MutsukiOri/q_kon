004e0642a9aa694847ba1bb6071269ee76832777
MutsukiOri <57990154+MutsukiOri@users.noreply.github.com>
2021-02-22 18:13:37 +0900

    画像

---
+++ pages/galleryneo.vue
@@ -0,0 +1,52 @@
+<template>
+    <v-container fluid>
+      <v-row>
+        <v-col
+          :cols=12
+          dense
+          class="pa-2"
+        >
+        <Top/>
+        </v-col>
+        <v-col
+          :cols=12
+          dense
+          class="pa-2"
+        >
+        <News/>
+        </v-col>
+        <v-col
+          :cols=12
+          dense
+          class="pa-2"
+        >
+        <Plan/>
+        </v-col>
+        <v-col
+          :cols=12
+          dense
+          class="pa-2"
+        >
+        <Calender/>
+        </v-col>
+      </v-row>
+    </v-container>
+</template>
+
+<script>
+  import Top from '@/components/Index/indextop.vue';
+  import News from '@/components/Index/news.vue';
+  import Plan from '@/components/Index/plan.vue';
+  import Calender from '@/components/Index/calender.vue'
+  export default {
+    components: {
+      Top,
+      News,
+      Plan,
+      Calender
+    },
+    data: () => ({
+
+    }),
+  }
+</script>
