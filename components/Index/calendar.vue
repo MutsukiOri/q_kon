e880f8700c6ee75f2707936030f8f6e48a616200
MutsukiOri <57990154+MutsukiOri@users.noreply.github.com>
2021-03-03 15:52:21 +0900

    fullcalendarでgoogleカレンダー取得

--- components/Index/calendar.vue
+++ components/Index/calendar.vue
@@ -10,250 +10,67 @@
             <v-card-title>
                 カレンダー
             </v-card-title>
-            <!-- <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Asia%2FTokyo&amp;src=bmVjZWQ4Zjc0MWFuNTcwOHRmb2o3Z25nZGdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23F4511E" style="border:solid 1px #777" width="100%" height="500" frameborder="0" scrolling="no"></iframe> -->
-            <v-row class="fill-height">
-                <v-col>
-                <v-sheet height="64">
-                    <v-toolbar
-                    color="#F5917E"
-                    dark
-                    flat
-                    >
-                    <v-btn
-                        outlined
-                        
-                        small
-                        class="mr-4"
-                        color="grey lighten-2"
-                        @click="setToday"
-                    >
-                        Today
-                    </v-btn>
-                    <v-btn
-                        fab
-                        text
-                        small
-                        dark
-                        color="grey lighten-2"
-                        @click="prev"
-                    >
-                        <v-icon small>
-                        mdi-chevron-left
-                        </v-icon>
-                    </v-btn>
-                    <v-btn
-                        fab
-                        text
-                        small
-                        color="grey lighten-2"
-                        @click="next"
-                    >
-                        <v-icon small>
-                        mdi-chevron-right
-                        </v-icon>
-                    </v-btn>
-                    <v-toolbar-title v-if="$refs.calendar">
-                        {{ $refs.calendar.title }}
-                    </v-toolbar-title>
-                    <v-spacer></v-spacer>
-                    <v-menu
-                        bottom
-                        right
-                    >
-                        <template v-slot:activator="{ on, attrs }">
-                        <v-btn
-                            outlined
-                            small
-                            color="grey lighten-2"
-                            v-bind="attrs"
-                            v-on="on"
-                        >
-                            <span>{{ typeToLabel[type] }}</span>
-                            <v-icon right>
-                            mdi-menu-down
-                            </v-icon>
-                        </v-btn>
-                        </template>
-                        <v-list dark color="#F5917E">
-                        <v-list-item @click="type = 'day'">
-                            <v-list-item-title>Day</v-list-item-title>
-                        </v-list-item>
-                        <v-list-item @click="type = 'week'">
-                            <v-list-item-title>Week</v-list-item-title>
-                        </v-list-item>
-                        <v-list-item @click="type = 'month'">
-                            <v-list-item-title>Month</v-list-item-title>
-                        </v-list-item>
-                        <v-list-item @click="type = '4day'">
-                            <v-list-item-title>4 days</v-list-item-title>
-                        </v-list-item>
-                        </v-list>
-                    </v-menu>
-                    </v-toolbar>
-                </v-sheet>
-                <v-sheet height="600">
-                    <v-calendar
-                    ref="calendar"
-                    v-model="focus"
-                    color="#F5917E"
-                    :events="events"
-                    :event-color="getEventColor"
-                    :type="type"
-                    @click:event="showEvent"
-                    @click:more="viewDay"
-                    @click:date="viewDay"
-                    @change="updateRange"
-                    ></v-calendar>
-                    <v-menu
-                    v-model="selectedOpen"
-                    :close-on-content-click="false"
-                    :activator="selectedElement"
-                    offset-x
-                    >
-                    <v-card
-                        color="grey lighten-4"
-                        min-width="350px"
-                        flat
-                    >
-                        <v-toolbar
-                        :color="selectedEvent.color"
-                        dark
-                        >
-                        <v-btn icon>
-                            <v-icon>mdi-pencil</v-icon>
-                        </v-btn>
-                        <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
-                        <v-spacer></v-spacer>
-                        <v-btn icon>
-                            <v-icon>mdi-heart</v-icon>
-                        </v-btn>
-                        <v-btn icon>
-                            <v-icon>mdi-dots-vertical</v-icon>
-                        </v-btn>
-                        </v-toolbar>
-                        <v-card-text>
-                        <span v-html="selectedEvent.details"></span>
-                        </v-card-text>
-                        <v-card-actions>
-                        <v-btn
-                            text
-                            color="secondary"
-                            @click="selectedOpen = false"
-                        >
-                            Cancel
-                        </v-btn>
-                        </v-card-actions>
-                    </v-card>
-                    </v-menu>
-                </v-sheet>
-                </v-col>
-            </v-row>
-          <!-- <FullCalendar :options="calendarOptions" /> -->
+          <v-card>
+          <FullCalendar :options="calendarOptions" :eventSources="eventSources"/>
+          </v-card>
         </v-container>
         
     </v-img>
     </v-card>
 </template>
 <script>
-// import FullCalendar from '@fullcalendar/vue'
-// import dayGridPlugin from '@fullcalendar/daygrid'
-// import interactionPlugin from '@fullcalendar/interaction'
+import FullCalendar from '@fullcalendar/vue'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+import timeGridPlugin from "@fullcalendar/timegrid";
+import googleCalendarPlugin from '@fullcalendar/google-calendar';
+import jaLocale from "@fullcalendar/core/locales/ja";
   export default {
       components: {
-        // FullCalendar,
+        FullCalendar,
         name: 'カレンダー',
       },
-    data (){
-      return{
-        // calendarOptions: {
-        //   plugins: [ dayGridPlugin, interactionPlugin ],
-        //   initialView: 'dayGridMonth'
-        // },
-        imgPath: 'https://cdn.vuetifyjs.com/images/cards/road.jpg',
-        
-        focus: '',
-        type: 'month',
-        typeToLabel: {
-          month: 'Month',
-          week: 'Week',
-          day: 'Day',
-          '4day': '4 Days',
+    data() {
+    return {
+      imgPath: 'https://picsum.photos/1920/1080?random',
+      calendarOptions: {
+        plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin, googleCalendarPlugin],
+        headerToolbar: {
+          left: 'prev today next',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek,timeGridDay'
         },
-        selectedEvent: {},
-        selectedElement: null,
-        selectedOpen: false,
-        events: [],
-        colors: ['blue', 'indigo', 'deep-purple', 'cyan'],
-        names: ['伊都教室練習', '西新練習', '椎木練習', '課外練習'],
-      }
-    },
-    mounted () {
-      this.$refs.calendar.checkChange()
-    },
-    methods: {
-      viewDay ({ date }) {
-        this.focus = date
-        this.type = 'day'
+        initialView: 'dayGridMonth',
+        locale:jaLocale,
+        editable: false,
+        selectable: true,
+        selectMirror: true,
+        dayMaxEvents: true,
+        weekends: true,
+        select: this.handleDateSelect,
+        eventsSet: this.handleEvents,
+        eventClick: this.handleEventClick,
+        eventsSet: this.handleEvents  ,
+        /* you can update a remote database when these fire:
+        eventAdd:
+        eventChange:
+        eventRemove:
+        */
+       googleCalendarApiKey: 'AIzaSyCwqHQ4OP160rtP8Rr5uS5j-8v9xfdl5EU',
+       eventSources: [
+        {
+          googleCalendarId: 'neced8f741an5708tfoj7gngdg@group.calendar.google.com',
+        },
+      ],
       },
-      getEventColor (event) {
-        return event.color
-      },
-      setToday () {
-        this.focus = ''
-      },
-      prev () {
-        this.$refs.calendar.prev()
-      },
-      next () {
-        this.$refs.calendar.next()
-      },
-      showEvent ({ nativeEvent, event }) {
-        const open = () => {
-          this.selectedEvent = event
-          this.selectedElement = nativeEvent.target
-          setTimeout(() => {
-            this.selectedOpen = true
-          }, 10)
-        }
+    }
+  },
+  methods: {
+    handleEvents(events) {},
+    handleWeekendsToggle() {},
+    handleDateSelect(selectInfo) {},
+    handleEventClick(clickInfo) {},
+  }
 
-        if (this.selectedOpen) {
-          this.selectedOpen = false
-          setTimeout(open, 10)
-        } else {
-          open()
-        }
-
-        nativeEvent.stopPropagation()
-      },
-      updateRange ({ start, end }) {
-        const events = []
-
-        const min = new Date(`${start.date}T00:00:00`)
-        const max = new Date(`${end.date}T23:59:59`)
-        const days = (max.getTime() - min.getTime()) / 86400000
-        const eventCount = this.rnd(days, days)
-
-        for (let i = 0; i < eventCount; i++) {
-          const allDay = this.rnd(0, 3) === 0
-          const firstTimestamp = this.rnd(min.getTime(), max.getTime())
-          const first = new Date(firstTimestamp - (firstTimestamp % 900000))
-          const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000
-          const second = new Date(first.getTime() + secondTimestamp)
-
-          events.push({
-            name: this.names[this.rnd(0, this.names.length - 1)],
-            start: first,
-            end: second,
-            color: this.colors[this.rnd(0, this.colors.length - 1)],
-            timed: !allDay,
-          })
-        }
-
-        this.events = events
-      },
-      rnd (a, b) {
-        return Math.floor((b - a + 1) * Math.random()) + a
-      },
-    },
   }
 </script>
